9fa221d5ab95d552ba308b4bcedbb73c0de94337
a3y3 <sohamssd@gmail.com>
2017-02-18 16:18:15 +0530

    Databases

--- Lab4/Database/app/src/main/java/com/example/student/database/ScrollingActivity.java
+++ Lab4/Database/app/src/main/java/com/example/student/database/ScrollingActivity.java
@@ -1,16 +1,22 @@
 package com.example.student.database;
 
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 public class ScrollingActivity extends AppCompatActivity {
 
+    private String DBNAME = "test.db";
+    private String TABLE = "MY_TABLE1";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,7 +24,30 @@ public class ScrollingActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        SQLiteDatabase db = openOrCreateDatabase(DBNAME, Context.MODE_PRIVATE,null);
+
+        db.execSQL("CREATE TABLE IF NOT EXISTS "+TABLE+"(email VARCHAR, firstName VARCHAR, lastName VARCHAR);");
+        db.execSQL("INSERT INTO " + TABLE + "(email, firstName,lastName) VALUES('xyz.abc1@manipal.edu','xyz','abc1')");
+        db.execSQL("INSERT INTO " + TABLE + "(email, firstName,lastName) VALUES('def2.ghi@manipal.edu','def2','ghi')");
+        db.execSQL("INSERT INTO " + TABLE + "(email, firstName,lastName) VALUES('aaaa.cccc@manipal.edu','aaaa','cccc')");
+
+        Cursor allRows = db.rawQuery("SELECT * FROM "+ TABLE,null);
+        Integer cIndex = allRows.getColumnIndex("email");
+        Integer cIndex1 = allRows.getColumnIndex("firstname");
+        Toast.makeText(getApplicationContext(),allRows.getCount()+" rows found",Toast.LENGTH_SHORT).show();
+
+        if(allRows.moveToFirst())
+        {
+            do {
+                String email = allRows.getString(0);
+                String FNAME = allRows.getString(1);
+                Toast.makeText(getApplicationContext(),email+""+FNAME, Toast.LENGTH_SHORT).show();
+            }while(allRows.moveToNext());
+        }
+        db.close();
+
+
+        FloatingActionButton fab = (FloatingActionButton)findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
