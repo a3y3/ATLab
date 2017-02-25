bd4051529e7ee196f97166e4d0d2f78410365843
a3y3 <sohamssd@gmail.com>
2017-02-25 16:38:26 +0530

    Add insert statements on click of button

--- Lab6/InvoiceMan/app/src/main/java/com/example/student/invoiceman/MainActivity.java
+++ Lab6/InvoiceMan/app/src/main/java/com/example/student/invoiceman/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.student.invoiceman;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,12 +10,22 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     private String TABLE_PRODUCT = "PRODUCT";
     private String TABLE_PRICE = "PRICE";
-    private String DB_NAME = "INVOICE_DB";
-
+    private String DB_NAME = "testdblab61";
+    private EditText productName;
+    private EditText productRegion;
+    private EditText productPrice;
+    private RadioGroup radioGroup1;
+    private RadioButton radioButton;
+    private int flag = 0;
+    SQLiteDatabase sqLiteDatabase;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,15 +33,25 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        /*SQLiteDatabase sqLiteDatabase = openOrCreateDatabase(DB_NAME, Context.MODE_PRIVATE, null);
 
-        sqLiteDatabase.execSQL("CREATE TABLE IF NOT EXISTS " + TABLE_PRODUCT + "(PRODUCT_ID INT, PRODUCT_NAME VARCHAR, REGION VARCHAR, IS_AVAILABLE VARCHAR)");
-        sqLiteDatabase.execSQL("INSERT INTO " + TABLE_PRODUCT + "VALUES 12, Toothbrush, Nashik, yes");
+
+
+        radioGroup1 = (RadioGroup)findViewById(R.id.radio_group_available);
+        radioGroup1.check(R.id.is_available_yes);
+        Button goButton = (Button)findViewById(R.id.button_go);
+        goButton.setOnClickListener(this);
+
+
+        sqLiteDatabase= openOrCreateDatabase(DB_NAME, Context.MODE_PRIVATE, null);
+        //sqLiteDatabase.execSQL("CREATE TABLE IF NOT EXISTS DOGGIE(NAME VARCHAR)");
+        sqLiteDatabase.execSQL("CREATE TABLE IF NOT EXISTS " + TABLE_PRODUCT + "(PRODUCT_ID INT PRIMARY KEY, PRODUCT_NAME VARCHAR, REGION VARCHAR, IS_AVAILABLE VARCHAR)");
+        //sqLiteDatabase.execSQL("CREATE TABLE IF NOT EXISTS " + TABLE_PRICE + "PRODUCT_ID INT, PRICE INT");
+        /*sqLiteDatabase.execSQL("INSERT INTO " + TABLE_PRODUCT + "VALUES 12, Toothbrush, Nashik, yes");
         sqLiteDatabase.execSQL("INSERT INTO " + TABLE_PRODUCT + "VALUES 13, Fiber Optic Cable, India, no");
         sqLiteDatabase.execSQL("INSERT INTO " + TABLE_PRODUCT + "VALUES 14, Gaming Mouse, USA, yes");
         sqLiteDatabase.execSQL("INSERT INTO " + TABLE_PRODUCT + "VALUES 15, 16GB RAM, India, yes");
 
-        sqLiteDatabase.execSQL("CREATE TABLE IF NOT EXISTS " + TABLE_PRICE + "PRODUCT_ID INT, PRICE INT");
+
         sqLiteDatabase.execSQL("INSERT INTO " + TABLE_PRICE + "VALUES 12,5000");
         sqLiteDatabase.execSQL("INSERT INTO " + TABLE_PRICE + "VALUES 13,40000" );
         sqLiteDatabase.execSQL("INSERT INTO " + TABLE_PRICE + "VALUES 14, 2000");
@@ -46,6 +68,25 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+    }
+
+    @Override
+    public void onClick(View view)
+    {
+        productName = (EditText)findViewById(R.id.edit_text_1);
+        productRegion = (EditText)findViewById(R.id.edit_text_2);
+        int selectedAvailability = radioGroup1.getCheckedRadioButtonId();
+        radioButton = (RadioButton)findViewById(selectedAvailability);
+        if(radioButton.getText().toString().equals("Available"))
+        {
+            flag = 1;
+        }
+        else
+            flag = 0;
+        productPrice = (EditText)findViewById(R.id.edit_text_3);
+
+        //sqLiteDatabase.execSQL("INSERT INTO "+TABLE_PRODUCT+"VALUES('");
+
     }
 
     @Override
