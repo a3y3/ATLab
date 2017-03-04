0caf0ff5e89c92de965b01bd178cf962432d9563
a3y3 <sohamssd@gmail.com>
2017-03-04 15:55:31 +0530

    Fix Lab 6

--- Lab6/InvoiceMan/app/src/main/java/com/example/student/invoiceman/MainActivity.java
+++ Lab6/InvoiceMan/app/src/main/java/com/example/student/invoiceman/MainActivity.java
@@ -20,7 +20,7 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     private String TABLE_PRODUCT = "PRODUCT";
     private String TABLE_PRICE = "PRICE";
-    private String DB_NAME = "testdblab61";
+    private String DB_NAME = "testdblab64";
     private EditText productName;
     private EditText productRegion;
     private EditText productPrice;
@@ -28,6 +28,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private RadioButton radioButton;
     private int flag = 0;
     SQLiteDatabase sqLiteDatabase;
+    Cursor cursor;
+    String pName,pRegion,pAvail;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,25 +44,60 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         radioGroup1.check(R.id.is_available_yes);
         Button goButton = (Button)findViewById(R.id.button_go);
         goButton.setOnClickListener(this);
+        Button updateButton = (Button)findViewById(R.id.update_button);
+        productName = (EditText)findViewById(R.id.edit_text_1);
+        productRegion = (EditText)findViewById(R.id.edit_text_2);
+
+        pName = productName.getText().toString();
+        pRegion = productRegion.getText().toString();
+        int selectedAvailability = radioGroup1.getCheckedRadioButtonId();
+        radioButton = (RadioButton)findViewById(selectedAvailability);
+
+        updateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pName = productName.getText().toString();
+                pRegion = productRegion.getText().toString();
+                sqLiteDatabase.execSQL("UPDATE "+TABLE_PRODUCT+" SET PRODUCT_REGION='"+pRegion+"' WHERE PRODUCT_NAME LIKE '"+pName+"'");
+                cursor = sqLiteDatabase.rawQuery("SELECT * FROM "+TABLE_PRODUCT+" NATURAL JOIN "+TABLE_PRICE,null);
+                Toast.makeText(getApplicationContext(),cursor.getCount()+" items", Toast.LENGTH_SHORT).show();
+                if(cursor.moveToFirst())
+                {
+                    do {
+                        String name = cursor.getString(1);
+                        String region = cursor.getString(2);
+                        Toast.makeText(getApplicationContext(),name+" "+region, Toast.LENGTH_SHORT).show();
+                    }while(cursor.moveToNext());
+                }
+            }
+        });
+
+        Button deleteButton = (Button)findViewById(R.id.delete_button);
+        deleteButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v) {
+                pName = productName.getText().toString();
+                sqLiteDatabase.execSQL("DELETE FROM "+TABLE_PRODUCT+" WHERE PRODUCT_NAME LIKE '"+pName+"'");
+
+                cursor = sqLiteDatabase.rawQuery("SELECT * FROM "+TABLE_PRODUCT+" NATURAL JOIN "+TABLE_PRICE,null);
+                Toast.makeText(getApplicationContext(),cursor.getCount()+" items", Toast.LENGTH_SHORT).show();
+                if(cursor.moveToFirst())
+                {
+                    do {
+                        String name = cursor.getString(1);
+                        String region = cursor.getString(2);
+                        Toast.makeText(getApplicationContext(),name+" "+region, Toast.LENGTH_SHORT).show();
+                    }while(cursor.moveToNext());
+                }
+            }
+        });
+
 
 
         sqLiteDatabase= openOrCreateDatabase(DB_NAME, Context.MODE_PRIVATE, null);
-        //sqLiteDatabase.execSQL("CREATE TABLE IF NOT EXISTS DOGGIE(NAME VARCHAR)");
-        sqLiteDatabase.execSQL("CREATE TABLE IF NOT EXISTS " + TABLE_PRODUCT + "(PRODUCT_ID INT PRIMARY KEY AUTOINCREMENT, PRODUCT_NAME VARCHAR, REGION VARCHAR, IS_AVAILABLE VARCHAR)");
-        sqLiteDatabase.execSQL("CREATE TABLE IF NOT EXISTS " + TABLE_PRICE + "(PRODUCT_ID INT PRIMARY KEY AUTOINCREMENT, PRICE INT)");
-        /*sqLiteDatabase.execSQL("INSERT INTO " + TABLE_PRODUCT + "VALUES 12, Toothbrush, Nashik, yes");
-        sqLiteDatabase.execSQL("INSERT INTO " + TABLE_PRODUCT + "VALUES 13, Fiber Optic Cable, India, no");
-        sqLiteDatabase.execSQL("INSERT INTO " + TABLE_PRODUCT + "VALUES 14, Gaming Mouse, USA, yes");
-        sqLiteDatabase.execSQL("INSERT INTO " + TABLE_PRODUCT + "VALUES 15, 16GB RAM, India, yes");
-
-
-        sqLiteDatabase.execSQL("INSERT INTO " + TABLE_PRICE + "VALUES 12,5000");
-        sqLiteDatabase.execSQL("INSERT INTO " + TABLE_PRICE + "VALUES 13,40000" );
-        sqLiteDatabase.execSQL("INSERT INTO " + TABLE_PRICE + "VALUES 14, 2000");
-        sqLiteDatabase.execSQL("INSERT INTO " + TABLE_PRICE + "VALUES 15, 6000"); //Discount.*/
-
-
-
+        sqLiteDatabase.execSQL("CREATE TABLE IF NOT EXISTS " + TABLE_PRODUCT + "(PRODUCT_ID INTEGER PRIMARY KEY AUTOINCREMENT, PRODUCT_NAME VARCHAR, REGION VARCHAR, IS_AVAILABLE VARCHAR)");
+        sqLiteDatabase.execSQL("CREATE TABLE IF NOT EXISTS " + TABLE_PRICE + "(PRODUCT_ID INTEGER PRIMARY KEY AUTOINCREMENT, PRICE INT)");
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -77,6 +114,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     {
         productName = (EditText)findViewById(R.id.edit_text_1);
         productRegion = (EditText)findViewById(R.id.edit_text_2);
+        String pName,pRegion,pAvail;
+        pName = productName.getText().toString();
+        pRegion = productRegion.getText().toString();
         int selectedAvailability = radioGroup1.getCheckedRadioButtonId();
         radioButton = (RadioButton)findViewById(selectedAvailability);
         if(radioButton.getText().toString().equals("Available"))
@@ -93,10 +133,18 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
         else
             isAvailable = "No";
-       // sqLiteDatabase.execSQL("INSERT INTO "+TABLE_PRODUCT+"(PRODUCT_NAME,REGION,IS_AVAILABLE) VALUES('"+productPrice+"','"+productRegion+"','"+isAvailable+"')");
-
-        Cursor cursor = sqLiteDatabase.rawQuery("SELECT * FROM "+TABLE_PRODUCT+" NATURAL JOIN "+TABLE_PRICE,null);
-        Toast.makeText(getApplicationContext(),cursor.getCount(), Toast.LENGTH_SHORT).show();
+        sqLiteDatabase.execSQL("INSERT INTO "+TABLE_PRODUCT+"(PRODUCT_NAME,REGION,IS_AVAILABLE) VALUES('"+pName+"','"+pRegion+"','"+isAvailable+"')");
+        sqLiteDatabase.execSQL("INSERT INTO "+TABLE_PRICE+"(PRICE) VALUES('"+productPrice+"')");
+        cursor = sqLiteDatabase.rawQuery("SELECT * FROM "+TABLE_PRODUCT+" NATURAL JOIN "+TABLE_PRICE,null);
+        Toast.makeText(getApplicationContext(),cursor.getCount()+" items", Toast.LENGTH_SHORT).show();
+        if(cursor.moveToFirst())
+        {
+            do {
+                String name = cursor.getString(1);
+                String region = cursor.getString(2);
+                Toast.makeText(getApplicationContext(),name+" "+region, Toast.LENGTH_SHORT).show();
+            }while(cursor.moveToNext());
+        }
     }
 
     @Override
