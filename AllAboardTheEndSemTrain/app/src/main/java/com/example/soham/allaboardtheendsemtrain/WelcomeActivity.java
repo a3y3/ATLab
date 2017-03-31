904b3a729e6e39a6c4de4207c13c497311c40eef
Soham Dongargaonkar <sohamssd@gmail.com>
2017-03-31 20:11:49 +0530

    Add AlertDialog.Builder

--- AllAboardTheEndSemTrain/app/src/main/java/com/example/soham/allaboardtheendsemtrain/WelcomeActivity.java
+++ AllAboardTheEndSemTrain/app/src/main/java/com/example/soham/allaboardtheendsemtrain/WelcomeActivity.java
@@ -1,22 +1,33 @@
 package com.example.soham.allaboardtheendsemtrain;
 
+import android.content.DialogInterface;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class WelcomeActivity extends AppCompatActivity implements View.OnClickListener{
     private SQLiteDatabase sqLiteDatabase;
     private EditText nameEditText;
     private EditText numberEditText;
     private Cursor cursor;
+    String dataList[];
+    ArrayAdapter<String> adapter;
+    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,14 +46,31 @@ public class WelcomeActivity extends AppCompatActivity implements View.OnClickLi
         });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+
         sqLiteDatabase = openOrCreateDatabase("test.db",MODE_PRIVATE,null);
         sqLiteDatabase.execSQL("CREATE TABLE IF NOT EXISTS DATA(ID INTEGER PRIMARY KEY AUTOINCREMENT, NAME TEXT, NUMBER TEXT)");
         nameEditText = (EditText)findViewById(R.id.nameEditText);
         numberEditText = (EditText)findViewById(R.id.numberEditText);
 
+        listView = (ListView)findViewById(R.id.list_view);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, final int position, long id) {
+
+                new AlertDialog.Builder(WelcomeActivity.this)
+                        .setTitle("Delete entry")
+                        .setMessage("Are you sure to delete this entry?")
+                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+
+                            }
+                        })
+                        .show();
+            }
+        });
         Button insertButton = (Button)findViewById(R.id.insertButton);
         insertButton.setOnClickListener(this);
-
     }
     @Override
     public void onClick(View view){
@@ -51,12 +79,17 @@ public class WelcomeActivity extends AppCompatActivity implements View.OnClickLi
             case R.id.insertButton:
                 String name = nameEditText.getText().toString();
                 String number = numberEditText.getText().toString();
-                sqLiteDatabase.execSQL("INSERT INTO DATA(NAME, NUMBER) VALUES '"+name+"', '"+ number+"'");
+                sqLiteDatabase.execSQL("INSERT INTO DATA(NAME, NUMBER) VALUES('"+name+"','"+ number+"')");
                 cursor = sqLiteDatabase.rawQuery("SELECT * FROM DATA", null);
-                Toast.makeText(this, ""+cursor.getCount(), Toast.LENGTH_SHORT).show();
-                Toast.makeText(this, "Okay", Toast.LENGTH_SHORT).show();
-
+                dataList= new String[cursor.getCount()];
+                cursor.moveToFirst();
+                for(int i =0; i < cursor.getCount();i++){
+                    dataList[i] = cursor.getString(1);
+                    cursor.moveToNext();
+                }
+                adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,android.R.id.text1, dataList);
+                listView.setAdapter(adapter);
+                adapter.notifyDataSetChanged();
         }
     }
-
 }
